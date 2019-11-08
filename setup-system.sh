b33c73a38e30211a1fb42364c378814791df0e99
Nicolás Ramírez <nramirez.uy@gmail.com>
2019-11-08 14:15:32 -0300

    feat: add setup-system

--- setup-system.sh
+++ setup-system.sh
@@ -2,42 +2,19 @@
 
 echo 'setup-system'
 
-USERNAME='&'
-while ! echo "$USERNAME" | grep "^[a-z_][a-z0-9_-]*$" > /dev/null 2>&1 \
-    || id -u "$USERNAME" > /dev/null 2>&1 ; do
-  read -p "username: " USERNAME
-done
-
-PASSWORD=''
-PASSWORD_TO_COMPARE='not a password'
-while ! [ "$PASSWORD" = "$PASSWORD_TO_COMPARE" ] ; do
-  read -sp "password: " PASSWORD
-  echo
-  read -sp "passwrod again: " PASSWORD_TO_COMPARE
-  echo
-done
-unset PASSWORD_TO_COMPARE
-
-# Register username, password and create home
-useradd -m -g wheel -s /bin/bash "$USERNAME" > /dev/null 2>&1 ||
-  useradd -m -G wheel "$USERNAME" && mkdir -p /home/"$USERNAME" &&
-    chown "$USERNAME":wheel /home/"$USERNAME"
-echo "$USERNAME":"$PASSWORD" | chpasswd
-unset PASSWORD
-
-# Refresh keyring
-pacman --noconfirm -Sy archlinux-keyring
-
-# Install basics
-pacman --noconfirm --needed -S base-devel git vim
+# Use all cores for compilation
+sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
 # Prettify pacman
 sed -i "s/^#Color/Color/" /etc/pacman.conf
 grep "ILoveCandy" /etc/pacman.conf > /dev/null ||
   sed -i "/^Color/a ILoveCandy" /etc/pacman.conf
 
-# Use all cores for compilation
-sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+# Refresh keyring
+pacman --noconfirm -Sy archlinux-keyring
+
+# Install basics
+pacman --noconfirm --needed -S base-devel git vim
 
 # Install yay
 su $USERNAME <<EOF
@@ -46,5 +23,5 @@ su $USERNAME <<EOF
   cd yay &&
   makepkg --noconfirm -si &&
   cd /tmp &&
-  rm -fr yay &&
+  rm -fr yay
 EOF
