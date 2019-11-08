2df1868c1831bbbe63eaa9bc6c3b3b09cb5e3add
Nicolás Ramírez <nramirez.uy@gmail.com>
2019-11-08 18:05:33 -0300

    feat: add setup-system

--- setup-system.sh
+++ setup-system.sh
@@ -3,12 +3,12 @@
 echo 'setup-system'
 
 # Use all cores for compilation
-sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+sudo sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
 # Prettify pacman
-sed -i "s/^#Color/Color/" /etc/pacman.conf
+sudo sed -i "s/^#Color/Color/" /etc/pacman.conf
 grep "ILoveCandy" /etc/pacman.conf > /dev/null ||
-  sed -i "/^Color/a ILoveCandy" /etc/pacman.conf
+  sudo sed -i "/^Color/a ILoveCandy" /etc/pacman.conf
 
 # Refresh keyring
 pacman --noconfirm -Sy archlinux-keyring
@@ -16,12 +16,26 @@ pacman --noconfirm -Sy archlinux-keyring
 # Install basics
 pacman --noconfirm --needed -S base-devel git vim
 
-# Install yay
-su $USERNAME <<EOF
-  cd /tmp &&
-  git clone https://aur.archlinux.org/yay.git &&
-  cd yay &&
-  makepkg --noconfirm -si &&
-  cd /tmp &&
-  rm -fr yay
-EOF
+# Install yay (AUR helper)
+cd /tmp
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd yay
+makepkg --noconfirm -si
+cd /tmp
+rm -fr yay
+
+pacmaninstall() {
+  pacman --noconfirm --needed -S "$1" 
+}
+
+# Install programs.csv
+while IFS=, read -r tag program comment; do
+  if [ "$tag" -eq "TAG" ]; then
+    continue
+  fi
+
+  echo "$program: $comment"
+  case "$tag" in
+    "PAC") pacmaninstall "$program" ;;
+  esac
+done < programs.csv;
