14bea954265960b1d25de0f0453ab9d5bfc1b2d0
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-06-09 20:35:06 -0300

    feat(nvim): Set ignorecase to true

--- src/tag-nvim/config/nvim/init.lua
+++ src/tag-nvim/config/nvim/init.lua
@@ -10,14 +10,15 @@ vim.opt.colorcolumn = '80'
 vim.opt.signcolumn = 'no'
 
 vim.opt.wrap = false
+vim.opt.scrolloff = 999 -- scroll from the middle of the screen
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
-vim.opt.scrolloff = 999 -- scroll from the middle of the screen
-
 vim.opt.virtualedit = "block"
 
 vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
