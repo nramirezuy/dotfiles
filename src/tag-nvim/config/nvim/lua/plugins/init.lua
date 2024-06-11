3d77bf2d6b92502339b63f019056a14adfe7f17f
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-06-10 23:59:31 -0300

    refactor(nvim): Move LSP to its own directory

---
+++ src/tag-nvim/config/nvim/lua/plugins/init.lua
@@ -0,0 +1,89 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({ variant = "moon" })
+            vim.cmd("colorscheme rose-pine")
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c",
+                    "hcl",
+                    "lua",
+                    "make",
+                    "proto",
+                    "python",
+                    "sql",
+                    "toml",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                },
+
+                auto_install = true,
+
+                highlight = {
+                    enable = true,
+                },
+
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<leader>ss",
+                        node_incremental = "<Leader>si",
+                        scope_incremental = "<Leader>sc",
+                        node_decremental = "<Leader>sd",
+                    }
+                },
+
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v',
+                            ['@function.outer'] = 'V',
+                            ['@class.outer'] = '<c-v>',
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("plugins.lspconfig")
+        end,
+    },
+})
