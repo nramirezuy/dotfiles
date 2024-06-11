890ea4b04a56209a1334f8564204c54471905510
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-06-11 00:23:44 -0300

    feat(nvim): Add telescope.nvim plugin

--- src/tag-nvim/config/nvim/lua/plugins/init.lua
+++ src/tag-nvim/config/nvim/lua/plugins/init.lua
@@ -67,9 +67,9 @@ require("lazy").setup({
                             ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
                         },
                         selection_modes = {
-                            ['@parameter.outer'] = 'v',
-                            ['@function.outer'] = 'V',
-                            ['@class.outer'] = '<c-v>',
+                            ["@parameter.outer"] = "v",
+                            ["@function.outer"] = "V",
+                            ["@class.outer"] = "<c-v>",
                         },
                         include_surrounding_whitespace = true,
                     },
@@ -84,6 +84,22 @@ require("lazy").setup({
         "neovim/nvim-lspconfig",
         config = function()
             require("plugins.lspconfig")
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+            vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+            vim.keymap.set(
+                "n",
+                "<leader>ps",
+                function()
+                    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+                end
+            )
         end,
     },
 })
