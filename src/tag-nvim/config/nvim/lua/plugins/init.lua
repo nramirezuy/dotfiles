4212a7342dcc5fdc805ccb285afdeb7728b11b77
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-06-14 22:16:39 -0300

    feat(nvim): Install cmp-cmdline

--- src/tag-nvim/config/nvim/lua/plugins/init.lua
+++ src/tag-nvim/config/nvim/lua/plugins/init.lua
@@ -102,35 +102,36 @@ require("lazy").setup({
     },
     {
         "hrsh7th/nvim-cmp",
+        dependencies = { "hrsh7th/cmp-cmdline" },
         config = function()
-              local cmp = require'cmp'
+              local cmp = require"cmp"
               cmp.setup({
                   window = {},
                   mapping = cmp.mapping.preset.insert({
-                      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                      ['<C-Space>'] = cmp.mapping.complete(),
-                      ['<C-e>'] = cmp.mapping.abort(),
-                      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                      ["<C-Space>"] = cmp.mapping.complete(),
+                      ["<C-e>"] = cmp.mapping.abort(),
+                      ["<CR>"] = cmp.mapping.confirm({ select = true }),
                   }),
                   sources = cmp.config.sources({
-                      { name = 'nvim_lsp' },
+                      { name = "nvim_lsp" },
                   }, {
-                      { name = 'buffer' },
+                      { name = "buffer" },
                   })
               })
-              cmp.setup.cmdline({ '/', '?' }, {
+              cmp.setup.cmdline({ "/", "?" }, {
                   mapping = cmp.mapping.preset.cmdline(),
                   sources = {
-                      { name = 'buffer' }
+                      { name = "buffer" }
                   }
               })
-              cmp.setup.cmdline(':', {
+              cmp.setup.cmdline(":", {
                   mapping = cmp.mapping.preset.cmdline(),
                   sources = cmp.config.sources({
-                      { name = 'path' }
+                      { name = "path" }
                   }, {
-                      { name = 'cmdline' }
+                      { name = "cmdline" }
                   }),
                   matching = { disallow_symbol_nonprefix_matching = false }
               })
