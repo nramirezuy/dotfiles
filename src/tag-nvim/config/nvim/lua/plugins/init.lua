0c037f6dd6d4f637e359c9c6ae0dc4e11225ed27
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-06-16 19:32:08 -0300

    feat(nvim): Better mappings for nvim-cmp

--- src/tag-nvim/config/nvim/lua/plugins/init.lua
+++ src/tag-nvim/config/nvim/lua/plugins/init.lua
@@ -108,11 +108,13 @@ require("lazy").setup({
               cmp.setup({
                   window = {},
                   mapping = cmp.mapping.preset.insert({
+                      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+                      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
                       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                       ["<C-f>"] = cmp.mapping.scroll_docs(4),
                       ["<C-Space>"] = cmp.mapping.complete(),
                       ["<C-e>"] = cmp.mapping.abort(),
-                      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                      ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                   }),
                   sources = cmp.config.sources({
                       { name = "nvim_lsp" },
