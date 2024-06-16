5cdfb5b7f50573afba299948ee96673f138ba010
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-06-16 19:32:58 -0300

    feat(nvim): Set LSP keymaps and autoformat on save

--- src/tag-nvim/config/nvim/lua/plugins/lspconfig/python.lua
+++ src/tag-nvim/config/nvim/lua/plugins/lspconfig/python.lua
@@ -1,14 +1,50 @@
 local lspconfig = require("lspconfig")
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+
 lspconfig.pylsp.setup({
     capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd(
+                "BufWritePre",
+                {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({ async = false })
+                    end,
+                }
+            )
+        end
+
+        -- Mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+        vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "<space>fo", vim.lsp.buf.format, bufopts)
+
+        if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end
+    end,
     settings = {
         pylsp = {
             plugins = {
                 ruff = {
                     enabled = true,
                     formatEnabled = true,
+                    format = { "I" },
                     lineLength = 79,
                 },
                 pylsp_mypy = {
