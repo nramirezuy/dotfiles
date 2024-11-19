dd49bba598b1ca97502b9fb29c41e8d253c390a8
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-11-19 14:45:14 -0300

    feat(nvim): Map quicklist basic navigation

--- src/tag-nvim/config/nvim/lua/keymaps.lua
+++ src/tag-nvim/config/nvim/lua/keymaps.lua
@@ -1,2 +1,5 @@
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "}q", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "{q", "<cmd>cprevious<CR>zz")
