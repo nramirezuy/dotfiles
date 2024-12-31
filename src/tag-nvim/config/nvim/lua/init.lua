26b7dacc417f5fed20a4f9bc82244813d729c7ec
Nicolás Ramírez <nramirez.uy@gmail.com>
2024-12-30 23:36:04 -0300

    fix(nvim): Remove duplicated lua_ls config

--- src/tag-nvim/config/nvim/lua/init.lua
+++ src/tag-nvim/config/nvim/lua/init.lua
@@ -83,38 +83,7 @@ require("lazy").setup({
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                on_init = function(client)
-                    local path = client.workspace_folders[1].name
-                    if (
-                        vim.loop.fs_stat(path..'/.luarc.json')
-                        or vim.loop.fs_stat(path..'/.luarc.jsonc')
-                    ) then
-                        return
-                    end
-
-                    client.config.settings.Lua = vim.tbl_deep_extend(
-                        "force",
-                        client.config.settings.Lua,
-                        {
-                            runtime = {
-                                version = 'LuaJIT'
-                            },
-                            workspace = {
-                                checkThirdParty = false,
-                                library = {
-                                    vim.env.VIMRUNTIME,
-                                    "${3rd}/luv/library",
-                                }
-                            }
-                        }
-                    )
-                end,
-                settings = {
-                    Lua = {}
-                }
-            })
+            require("plugins.lspconfig")
         end,
     },
 })
